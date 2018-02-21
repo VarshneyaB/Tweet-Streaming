f4bc54e5277a5be2aadfea5c60db553f5e5c0a32
VarshneyaB <36578000+VarshneyaB@users.noreply.github.com>
2018-02-21 09:02:08 +0530

    Delete code.py

--- code.py
+++
@@ -1,9 +0,0 @@
-ckey="WAybii2PCWwRgQnso5yaIR1Xk"
-csecret="M7aHF5gGfv2AlMz0us9Bvp8ZchFst9t6AeeXzAD2WdvaLmCe4v"
-from pprint import pprint
-
-from API_1 import tweetStream
-t = tweetStream()
-t.setStorage("ex1")
-t.setAuth(ckey,csecret)
-t.startStreaming("modi",25)
