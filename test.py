eaccb3e63a19e58ce7f63f3810ee9cca01834f5d
VarshneyaB <36578000+VarshneyaB@users.noreply.github.com>
2018-02-21 09:03:33 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pprint import pprint
-conec = MongoClient()
-base = conec['twitterData']
-base.tweets.insert_one({"name" : "Modi","twitter" : "Lakaka"})
-x = base.docs.find_one({"name" : "Modi"})
-pprint(base)
-pprint(x)
